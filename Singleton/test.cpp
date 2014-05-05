c81e22a498ed6fa45060191768fd05b6216c038b
KevinFu <kevinfu@KevinHp.(none)>
2014-05-05 22:31:08 +0800

    Common Singleton, implemented by template

--- Singleton/test.cpp
+++ Singleton/test.cpp
@@ -1,29 +1,49 @@
-#include <boost/atomic.hpp>
-#include <boost/thread/mutex.hpp>
-
-class X {
+#include <iostream>
+#include <pthread.h>
+using namespace std;
+template <class T>
+class Singleton
+{
 public:
-  static X * instance()
-  {
-    X * tmp = instance_.load(boost::memory_order_consume);
-    if (!tmp) {
-      boost::mutex::scoped_lock guard(instantiation_mutex);
-      tmp = instance_.load(boost::memory_order_consume);
-      if (!tmp) {
-        tmp = new X;
-        instance_.store(tmp, boost::memory_order_release);
-      }
+    Singleton()
+    {
+
     }
-    return tmp;
-  }
+
+    static T* GetInstance()
+    {
+       if(m_pInstance == NULL)
+        {
+            pthread_mutex_lock(&m_Mutex);
+            if(m_pInstance == NULL)
+            {
+                T* temp = new T;
+                m_pInstance = temp;
+            }
+            pthread_mutex_unlock(&m_Mutex);
+        }
+        return m_pInstance;
+    }
 private:
-  static boost::atomic<X *> instance_;
-  static boost::mutex instantiation_mutex;
+    static T* m_pInstance;
+    static pthread_mutex_t m_Mutex;
 };
 
-boost::atomic<X *> X::instance_(0);
+template<class T>
+T* Singleton<T>::m_pInstance = NULL;
+
+template<class T>
+pthread_mutex_t Singleton<T>::m_Mutex;
+
+class Test:public Singleton<Test>
+{
+    
+};
 
 int main()
 {
-    X *x = X::instance();
+    Test *test1 = Test::GetInstance();
+    Test *test2 = Test::GetInstance();
+    cout<<"test1:"<<test1<<endl<<"test2:"<<test2<<endl;
+    return 0;
 }
