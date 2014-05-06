beae9ba127b3445b5cfbdf90fbc21c7ad0f1c5c3
kevin-shanghai <kevinfu1985@gmail.com>
2014-05-06 22:25:15 +0800

    Singleton for multithread

--- Singleton/test.cpp
+++ Singleton/test.cpp
@@ -1,3 +1,6 @@
+/**********************************************
+ ******* g++ test.cpp -lpthread****************
+ * *******************************************/
 #include <iostream>
 #include <pthread.h>
 using namespace std;
@@ -9,41 +12,65 @@ public:
     {
 
     }
-
     static T* GetInstance()
     {
        if(m_pInstance == NULL)
         {
-            pthread_mutex_lock(&m_Mutex);
+            //pthread_mutex_t mutex;
+            pthread_mutex_lock(&mutex);
             if(m_pInstance == NULL)
             {
                 T* temp = new T;
                 m_pInstance = temp;
             }
-            pthread_mutex_unlock(&m_Mutex);
+            pthread_mutex_unlock(&mutex);
         }
         return m_pInstance;
     }
 private:
     static T* m_pInstance;
-    static pthread_mutex_t m_Mutex;
+    static pthread_mutex_t mutex;
 };
 
 template<class T>
 T* Singleton<T>::m_pInstance = NULL;
 
 template<class T>
-pthread_mutex_t Singleton<T>::m_Mutex;
+pthread_mutex_t Singleton<T>::mutex;
 
 class Test:public Singleton<Test>
 {
-    
+public:
+    Test()
+    {
+        cout<<"Test::Test()."<<endl;
+    }
 };
+
+void* thread1(void*)
+{
+    cout<<"In thread1."<<endl;
+    Test* test1 = Test::GetInstance();
+}
+
+void* thread2(void *)
+{
+    cout<<"In thread2."<<endl;
+    Test* test2 = Test::GetInstance();
+}
 
 int main()
 {
-    Test *test1 = Test::GetInstance();
-    Test *test2 = Test::GetInstance();
-    cout<<"test1:"<<test1<<endl<<"test2:"<<test2<<endl;
+    const unsigned int thread_num = 20;
+    pthread_t thread_id[thread_num];
+    for(unsigned int i = 0; i<thread_num;i++)
+    {
+        pthread_create(&thread_id[i], NULL, thread1, NULL);
+    }
+    for(unsigned int i = 0; i<thread_num;i++)
+    {
+        pthread_create(&thread_id[i], NULL, thread2, NULL);
+    }
+    sleep(1);
     return 0;
 }
